efd3c67a3fb5a46d9161d06e035cdce5160c0d80
RestartFU <zerkoming@gmail.com>
2023-08-01 18:24:29 -0400

    HandleChat

--- moyai/user/handler.go
+++ moyai/user/handler.go
@@ -5,7 +5,11 @@ import (
 	"github.com/df-mc/dragonfly/server/event"
 	"github.com/df-mc/dragonfly/server/item"
 	"github.com/df-mc/dragonfly/server/player"
+	"github.com/df-mc/dragonfly/server/player/chat"
 	"github.com/df-mc/dragonfly/server/world"
+	"github.com/moyai-network/practice/moyai/data"
+	"regexp"
+	"strings"
 )
 
 type Handler struct {
@@ -16,6 +20,39 @@ type Handler struct {
 func NewHandler(p *player.Player) *Handler {
 	h := &Handler{p: p}
 	return h
+}
+
+var (
+	// tlds is a list of top level domains used for checking for advertisements.
+	tlds = [...]string{".me", ".club", "www.", ".com", ".net", ".gg", ".cc", ".net", ".co", ".co.uk", ".ddns", ".ddns.net", ".cf", ".live", ".ml", ".gov", "http://", "https://", ",club", "www,", ",com", ",cc", ",net", ",gg", ",co", ",couk", ",ddns", ",ddns.net", ",cf", ",live", ",ml", ",gov", ",http://", "https://", "gg/"}
+	// emojis is a map between emojis and their unicode representation.
+	emojis = strings.NewReplacer(
+		":l:", "\uE107",
+		":skull:", "\uE105",
+		":fire:", "\uE108",
+		":eyes:", "\uE109",
+		":clown:", "\uE10A",
+		":100:", "\uE10B",
+		":heart:", "\uE10C",
+	)
+)
+
+// formatRegex is a regex used to clean color formatting on a string.
+var formatRegex = regexp.MustCompile(`§[\da-gk-or]`)
+
+// HandleChat ...
+func (h *Handler) HandleChat(ctx *event.Context, message *string) {
+	ctx.Cancel()
+
+	u, ok := data.LoadUser(h.p.Name())
+	if !ok {
+		return
+	}
+
+	*message = emojis.Replace(*message)
+	r := u.Roles.Highest()
+
+	_, _ = chat.Global.WriteString(r.Chat(h.p.Name(), *message))
 }
 
 // HandleFoodLoss ...
